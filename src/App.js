0ab19a33b0e11ebdc57181155a03c8ca07e2cbd9
soph254 <sophiejones591@gmail.com>
2021-06-24 16:03:39 +0100

    added logo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import './App.css';
 import Footer from "./Footer";
 import Dictionary from "./Dictionary";
+import knowledge from "./Knowledge.png";
 
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-          <h1 Dictionary ></h1>
+        <header className="App-header">
+          <img src={knowledge} className="KnowledgeIcon" alt="icon" />
+          <h1> Dictionary </h1>
+
+          </header>
     
 
         <main>
